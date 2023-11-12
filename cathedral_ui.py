ea11ff5c70a3c657a42e08f4a5a665e1a0cd0a71
James <james.crook@fourthfloor.co>
2023-11-12 22:42:07 +0000

    modularised code

--- cathedral_ui.py
+++ cathedral_ui.py
@@ -1,6 +1,81 @@
 import bpy
 import math
 
+# Function to create the base object of the grid
+def create_base(rows, columns, cube_size_x, cube_size_y, cube_size_z, margin, base_height, gap):
+    # Create a cube as the base object
+    bpy.ops.mesh.primitive_cube_add(size=1)
+    base_obj = bpy.context.object
+    
+    # Scale the base object based on the grid dimensions and margin
+    base_obj.scale.x = rows * (cube_size_y + gap)
+    base_obj.scale.y = columns * (cube_size_x + gap)
+    base_obj.scale.z = base_height
+
+    # Position the base object to align the corner with the corner of the grid
+    base_obj.location.y = (columns * (cube_size_x + gap) - gap) / 2
+    base_obj.location.x = (rows * (cube_size_y + gap) - gap) / 2
+    base_obj.location.z = base_height
+
+    # Rotate the base object to ensure it's flat
+    base_obj.rotation_euler[0] = 0
+
+    return base_obj
+
+# Function to create an individual cube in the grid
+def create_cube(x_pos, y_pos, cube_size_x, cube_size_y, cube_size_z, base_height):
+    # Create a cube with specified dimensions
+    bpy.ops.mesh.primitive_cube_add(size=1)
+    cube_obj = bpy.context.object
+    cube_obj.scale.x = cube_size_x
+    cube_obj.scale.y = cube_size_y
+    cube_obj.scale.z = cube_size_z
+    
+    # Position the cube in the grid
+    cube_obj.location.x = x_pos + cube_size_x / 2
+    cube_obj.location.y = y_pos + cube_size_y / 2
+    cube_obj.location.z = (cube_size_z + base_height)
+
+    return cube_obj
+
+# Function to add a bevel modifier to round the corners of a cube
+def add_bevel_modifier(cube_obj, cell_radius_radians):
+    # Add a bevel modifier to the cube
+    bpy.ops.object.modifier_add(type='BEVEL')
+    bevel_modifier = cube_obj.modifiers[-1]
+    bevel_modifier.segments = 3
+    bevel_modifier.width = cell_radius_radians
+
+# Function to create the entire grid of cubes with rounded corners
+def create_grid(rows, columns, cube_size_x, cube_size_y, cube_size_z, margin, base_height, cell_radius_degrees):
+    gap = margin * 2
+    cell_radius_radians = math.radians(cell_radius_degrees)
+
+    # Create the base object of the grid
+    base_obj = create_base(rows, columns, cube_size_x, cube_size_y, cube_size_z, margin, base_height, gap)
+
+    # Iterate through the rows and columns to create individual cubes
+    for i in range(rows):
+        for j in range(columns):
+            x_pos = i * (cube_size_x + gap)
+            y_pos = j * (cube_size_y + gap)
+
+            # Create an individual cube in the grid
+            cube_obj = create_cube(x_pos, y_pos, cube_size_x, cube_size_y, cube_size_z, base_height)
+            
+            # Add a bevel modifier to round the corners of the cube
+            add_bevel_modifier(cube_obj, cell_radius_radians)
+            
+            # Deselect the cube
+            cube_obj.select_set(False)
+
+# Function to execute the creation of the grid based on scene properties
+def execute_create_grid(context):
+    scene = context.scene
+    create_grid(scene.grid_rows, scene.grid_columns, scene.cube_size_x, scene.cube_size_y,
+                scene.cube_size_z, scene.margin, scene.base_height, scene.cell_radius_degrees)
+
+# UI Panel for displaying grid settings
 class GridSettingsPanel(bpy.types.Panel):
     bl_label = "Grid Settings"
     bl_idname = "PT_GridSettings"
@@ -11,7 +86,6 @@ class GridSettingsPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
-        # Define the properties for the grid settings
         scene = context.scene
         layout.prop(scene, "grid_rows")
         layout.prop(scene, "grid_columns")
@@ -22,69 +96,18 @@ class GridSettingsPanel(bpy.types.Panel):
         layout.prop(scene, "base_height")
         layout.prop(scene, "cell_radius_degrees")
 
-        # Add a button to create the grid
         layout.operator("object.create_grid")
 
+# Operator for creating the grid
 class CreateGridOperator(bpy.types.Operator):
     bl_idname = "object.create_grid"
     bl_label = "Create Grid"
 
     def execute(self, context):
-        # Get the grid parameters from the scene properties
-        scene = context.scene
-        rows = scene.grid_rows
-        columns = scene.grid_columns
-        cube_size_x = scene.cube_size_x
-        cube_size_y = scene.cube_size_y
-        cube_size_z = scene.cube_size_z
-        margin = scene.margin
-        base_height = scene.base_height
-        cell_radius_degrees = scene.cell_radius_degrees
-        cell_radius_radians = math.radians(cell_radius_degrees)
-        gap = margin * 2
-
-        # Create the base
-        bpy.ops.mesh.primitive_cube_add(size=1)
-        base_obj = bpy.context.object
-        base_obj.scale.x = rows * (cube_size_y + gap)
-        base_obj.scale.y = columns * (cube_size_x + gap)
-        base_obj.scale.z = base_height
-
-        # Position the base to align the corner with the corner of the grid
-        base_obj.location.y = (columns * (cube_size_x + gap) - gap) / 2
-        base_obj.location.x = (rows * (cube_size_y + gap) - gap) / 2
-        base_obj.location.z = base_height
-
-        # Rotate the base to zero degrees in the X-axis (ensure the base is flat)
-        base_obj.rotation_euler[0] = 0
-
-        # Create the grid of cubes with rounded corners
-        for i in range(rows):
-            for j in range(columns):
-                x_pos = i * (cube_size_x + gap)
-                y_pos = j * (cube_size_y + gap)
-
-                # Create a new cube with specified dimensions
-                bpy.ops.mesh.primitive_cube_add(size=1)
-                cube_obj = bpy.context.object
-                cube_obj.scale.x = cube_size_x
-                cube_obj.scale.y = cube_size_y
-                cube_obj.scale.z = cube_size_z
-                cube_obj.location.x = x_pos + cube_size_x / 2
-                cube_obj.location.y = y_pos + cube_size_y / 2
-                cube_obj.location.z = (cube_size_z + base_height)
-
-                # Add a bevel modifier to round the corners
-                bpy.ops.object.modifier_add(type='BEVEL')
-                bevel_modifier = cube_obj.modifiers[-1]
-                bevel_modifier.segments = 3  # You can adjust the number of segments for smoother corners
-                bevel_modifier.width = cell_radius_radians
-
-                # Deselect the objects
-                cube_obj.select_set(False)
-
+        execute_create_grid(context)
         return {'FINISHED'}
 
+# Register and unregister functions
 def register():
     bpy.utils.register_class(GridSettingsPanel)
     bpy.utils.register_class(CreateGridOperator)
